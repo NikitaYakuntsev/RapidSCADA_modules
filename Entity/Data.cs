0e08696185ad7ea98009bd5e54c49fda879c72ed
Kit <nothing_special>
2016-02-05 19:54:18 +0300

    Entity module skeleton

---
+++ Entity/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Entity
+{
+    class Data
+    {
+        public virtual int Id { get; set; }
+        public virtual String Name { get; set; }
+    }
+}
